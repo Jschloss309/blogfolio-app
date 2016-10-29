7cc1d046a44bb48eebc8253cdfb3898bb96fbcbe
Jschloss309 <Jschloss309@gmail.com>
2016-10-28 21:24:54 -0600

    Add friendly_id gem & setup friendly_id on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,50 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-  def index
-  end
+	def index
+		@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
+	end
 
-  def new
-    @post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def create
-    @post = Post.new post_params
+	def create
+		@post = Post.new post_params
 
-    if @post.save
-      redirect_to @post, notice: "Hellz Yeah, Jonathon! Your article was successfully saved!"
-    else
-      render 'new', notice: "Oh no, Jonathon! I was unable to save your post."
-    end
-  end
+		if @post.save
+			redirect_to @post, notice: "Hellz yeah, Jonathon! Your article was successfully saved!"
+		else
+			render 'new', notice: "Oh no, Jonathon! I was unable to save your post."
+		end
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def update
-    if @post.update post_params
-      redirect_to @post, notice: "Huzzah! Your article was successfully saved!"
-    else
-      render 'edit'
-    end
-  end
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Huzzah! Your article was successfully saved!"
+		else
+			render 'edit'
+		end
+	end
 
-  def destroy
-    @post.destroy
-    redirect_to posts_path
-  end
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
 
-  private
+	private
 
-  def post_params
-    params.require(:post).permit(:title, :content)
-  end
+	def post_params
+		params.require(:post).permit(:title, :content, :slug)
+	end
 
-  def find_post
-    @post = Post.find(params[:id])
-  end
+	def find_post
+		@post = Post.friendly.find(params[:id])
+	end
 end
